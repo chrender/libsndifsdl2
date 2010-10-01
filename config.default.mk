b16678ac01844aa45e7bd5aea6f2a876b7e8439f
chrender <chrender>
2010-10-01 06:17:47 +0000

    - Cleaned up .mk files.

--- config.default.mk
+++ config.default.mk
@@ -5,18 +5,26 @@ CC = gcc
 AR = ar
 CFLAGS = -Wall -Wextra
 
-INSTALL_PREFIX = $(DESTDIR)/usr
-
-# If defined, install goes to "$(INSTALL_PREFIX)/($FIZMO_BIN_DIR)" instead of
-# "(INSTALL_PREFIX)/games" (usually use to subsitute "bin" for "games").
-#FIZMO_BIN_DIR = games
+ifneq ($(DESTDIR),)
+INSTALL_PREFIX = $(DESTDIR)
+else
+INSTALL_PREFIX = /usr/local
+endif
 
 DEFAULT_PREFIX = /usr
 DEFAULT_LIB_PREFIX = $(DEFAULT_PREFIX)/lib
 DEFAULT_INC_PREFIX = $(DEFAULT_PREFIX)/include
 
+FIZMO_INC_DIR = $(INSTALL_PREFIX)/include
+FIZMO_LIB_DIR = $(INSTALL_PREFIX)/lib
+
 SDL_INC_DIR = $(DEFAULT_INC_PREFIX)
 SDL_LIB_DIR = $(DEFAULT_LIB_PREFIX)
+
+# These lines are used for providing extra lib (dirs), currently used
+# for Mac OS.
+EXTRA_LIB_DIRS =
+EXTRA_LIBS =
 
 # If you have libsndfile available, fizmo will be able to read AIFF from
 # blorbs:
@@ -31,25 +39,11 @@ EXTRA_LIBS =
 # This adds an -O2 flag (usually okay):
 ENABLE_OPTIMIZATION = 1
 
-# If you're building a "dumb" interface like the CGI-interface (this
-# runs the minizork-demo on the webpage, take a look at src/cgi) you
-# may want to save memory and cpu by uncommenting the following lines:
-#DISABLE_BLOCKBUFFER = 1
-#DISABLE_COMMAND_HISTORY = 1
-#DISABLE_OUTPUT_HISTORY = 1
-
-
 # Debug-Flags:
 
 # Uncomment to fill your harddisk _very_ fast:
 #ENABLE_TRACING = 1
 
-# Used for the strictz-test:
-#ENABLE_STRICT_Z = 1
-
 # Add GDB symbols, only useful for debuggong:
 #ENABLE_GDB_SYMBOLS = 1
-
-# Throws sigfault on error for emergency backtrace (usually never needed):
-#THROW_SIGFAULT_ON_ERROR = 1
 
--- config.macports.mk
+++ config.macports.mk
@@ -5,11 +5,16 @@ CC = gcc
 AR = ar
 CFLAGS = -Wall -Wextra
 
+ifneq ($(DESTDIR),)
+INSTALL_PREFIX = $(DESTDIR)
+else
+INSTALL_PREFIX = /usr/local
+endif
+INSTALL_PREFIX = /Users/chrender/opt/fizmo
+
 DEFAULT_PREFIX = /opt/local
 DEFAULT_LIB_PREFIX = $(DEFAULT_PREFIX)/lib
 DEFAULT_INC_PREFIX = $(DEFAULT_PREFIX)/include
-
-INSTALL_PREFIX = /Users/chrender/opt/fizmo
 
 FIZMO_INC_DIR = $(INSTALL_PREFIX)/include
 FIZMO_LIB_DIR = $(INSTALL_PREFIX)/lib
@@ -17,7 +22,8 @@ FIZMO_LIB_DIR = $(INSTALL_PREFIX)/lib
 SDL_INC_DIR = $(DEFAULT_INC_PREFIX)
 SDL_LIB_DIR = $(DEFAULT_LIB_PREFIX)
 
-# These lines are used for providing extra lib (dirs), used for Mac OS.
+# These lines are used for providing extra lib (dirs), currently used
+# for Mac OS.
 EXTRA_LIB_DIRS = -L/opt/local/lib
 EXTRA_LIBS = -Wl,-framework,Cocoa
 
@@ -30,24 +36,11 @@ SNDFILE_LIB_DIR = $(DEFAULT_LIB_PREFIX)
 # This adds an -O2 flag (usually okay):
 ENABLE_OPTIMIZATION = 1
 
-# If you're building a "dumb" interface like the CGI-interface (this
-# runs the minizork-demo on the webpage, take a look at src/cgi) you
-# may want to save memory and cpu by uncommenting the following lines:
-#DISABLE_BLOCKBUFFER = 1
-#DISABLE_COMMAND_HISTORY = 1
-#DISABLE_OUTPUT_HISTORY = 1
-
 # Debug-Flags:
 
 # Uncomment to fill your harddisk _very_ fast:
 ENABLE_TRACING = 1
 
-# Used for the strictz-test:
-#ENABLE_STRICT_Z = 1
-
 # Add GDB symbols, only useful for debuggong:
 ENABLE_GDB_SYMBOLS = 1
-
-# Throws sigfault on error for emergency backtrace (usually never needed):
-#THROW_SIGFAULT_ON_ERROR = 1
 
